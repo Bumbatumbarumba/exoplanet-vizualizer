b31c0f09b6b280acd431812b25fbb4900248ec31
Bartosz <bartosz_@live.ca>
2020-01-31 16:08:21 -0500

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/App.css';
+
+const App = () => {
+  return (
+    <h1>Exoplanet Visualizer</h1>
+  );
+}
+
+export default App;
---
+++ src/helpers/nasaApiHelper.js
@@ -0,0 +1,20 @@
+const BASE_API = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?"
+const CONFIRMED_PLANETS_TABLE = "table=exoplanets"
+
+
+// pl_hostname
+// pl_letter
+// pl_name
+// pl_pnum
+// pl_orbper
+// pl_orbsmax
+// pl_orbeccen
+// pl_masse
+// pl_rade
+// pl_mnum
+// st_mass
+// st_rad
+// st_logg
+// st_dens
+// st_age
+// st_vsini
